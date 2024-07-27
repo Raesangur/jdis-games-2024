0b81c82d08f8e7d035aa4b4e1ef7961a42c1f67e
raesangur <pascalemmanuel.lachance@gmail.com>
2024-07-27 12:06:16 -0400

    aggressive, going to coins directly

--- python/src/bot.py
+++ python/src/bot.py
@@ -4,7 +4,7 @@ from typing import List, Union
 
 from core.action import MoveAction, ShootAction, RotateBladeAction, SwitchWeaponAction, SaveAction
 from core.consts import Consts
-from core.game_state import GameState, PlayerWeapon, Point
+from core.game_state import GameState, PlayerWeapon, Point, Coin
 from core.map_state import MapState
 
 
@@ -50,18 +50,25 @@ class MyBot:
                                 (en): The state of the game.   
         """
         print(f"Current tick: {game_state.current_tick}")
-        players = [p for p in game_state.players]
-        print(players)
-        player = [p for p in players if p.name == "bon-matin"][0]
+        player = [p for p in game_state.players if p.name == "bon-matin"][0]
         print(player.health)
 
+        coin = self.find_closest_coin(player, game_state.coins)
+
         actions = [
-            MoveAction((10.0, 11.34)),
-            ShootAction((11.2222, 13.547)),
-            SwitchWeaponAction(PlayerWeapon.PlayerWeaponBlade),
-            SaveAction(b"Bon Matin"),
+            #SwitchWeaponAction(PlayerWeapon.PlayerWeaponBlade),
+            #RotateBladeAction((game_state.current_tick % 8) * math.pi / 4),
+            MoveAction((coin.pos.x, coin.pos.y)),
         ]
 
+        ennemy, ennemyDist = self.find_closest_player(player, game_state.players) 
+
+        if (ennemyDist < 15):
+            if player.playerWeapon != 1:
+                actions.append(SwitchWeaponAction(PlayerWeapon.PlayerWeaponCanon))
+
+            actions.append(ShootAction((ennemy.pos.x, ennemy.pos.y)))
+        
         return actions
     
 
@@ -79,29 +86,6 @@ class MyBot:
         # 2- store wall
         array = [[0] * 100 for _ in range(100)]
         
-
-    def on_start(self, map_state: MapState):
-        """
-        (fr) Cette méthode est appelée une seule fois au début de la partie. Vous pouvez y définir des
-            actions à effectuer au début de la partie.
-
-        Arguments:
-            map_state (MapState): (fr) L'état de la carte.
-        """
-        self.__map_state = map_state
-        self.old_position = 0
-        pass
-
-
-    def on_end(self):
-        """
-        (fr) Cette méthode est appelée une seule fois à la fin de la partie. Vous pouvez y définir des
-            actions à effectuer à la fin de la partie.
-        """
-        pass
-
-
-
 
     def rotate_blade(angle):
         if angle == "UP":
@@ -130,3 +114,53 @@ class MyBot:
             dy = 0
 
         return ShootAction(currentPos.x + dx, currentPos.y + dy)
+
+
+
+    def on_start(self, map_state: MapState):
+        """
+        (fr) Cette méthode est appelée une seule fois au début de la partie. Vous pouvez y définir des
+            actions à effectuer au début de la partie.
+
+        Arguments:
+            map_state (MapState): (fr) L'état de la carte.
+        """
+        self.__map_state = map_state
+        self.old_position = 0
+        pass
+
+
+    def on_end(self):
+        """
+        (fr) Cette méthode est appelée une seule fois à la fin de la partie. Vous pouvez y définir des
+            actions à effectuer à la fin de la partie.
+        """
+        pass
+
+    def find_closest_coin(self, player, coins):
+        minDistance = 10000
+        bestCoin = None
+
+        for coin in coins:
+            distance = math.dist([player.pos.x, player.pos.y], [coin.pos.x, coin.pos.y])
+
+            if distance < minDistance:
+                bestCoin = coin
+                minDistance = distance
+
+        return bestCoin
+
+    def find_closest_player(self, player, players):
+        minDistance = 10000
+        bestPlayer = None
+
+        for p in players:
+            distance = math.dist([player.pos.x, player.pos.y], [p.pos.x, p.pos.y])
+            if distance == 0:
+                continue
+
+            if distance < minDistance:
+                bestPlayer = p
+                minDistance = distance
+
+        return bestPlayer, minDistance
